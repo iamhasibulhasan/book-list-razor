8d15540e9c3bc7ed160074a98812704ecc42dac7
imhasibulhasan <hashibul218@gmail.com>
2022-09-30 16:19:09 +0600

    add method

---
+++ BookListRazor/Migrations/20220930005645_newcolumnaddin book.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookListRazor.Migrations
+{
+    public partial class newcolumnaddinbook : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Book",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ISBN",
+                table: "Book",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Book");
+
+            migrationBuilder.DropColumn(
+                name: "ISBN",
+                table: "Book");
+        }
+    }
+}
